a07d3890031e6062a40f78cabbd257484f4c3942
LKen <liangchuq@163.com>
2018-12-13 20:58:41 +0800

    修改hangup

--- server/controller/socket/index.js
+++ server/controller/socket/index.js
@@ -16,7 +16,8 @@ const REJECT_TYPE = {
   '5003': '对方不存在',
   '5004': '正在拨打中，请勿重复拨打',
   '5005': '当前用户不再通话中，错误请求',
-  '5006': '对方拒绝通话'
+  '5006': '对方拒绝通话',
+  '5007': '对方正忙，请稍后在拨'
 }
 
 const ERROR_TYPE = {
@@ -36,11 +37,11 @@ const eventType = {
   call: Symbol('call'), // 代理转发
   accept: Symbol('accept'), // 代理转发
   reject: Symbol('reject'), // 代理转发
-  leave: Symbol('leave') // 代理转发
+  hangup: Symbol('hangup') // 代理转发
 }
 
 function handleMessage(data, socket) {
-  const { event, message } = data
+  const { event, message, success } = data
   switch (eventType[event]) {
     case eventType.offer:
       handleOffer.bind(this, socket)(message)
@@ -63,13 +64,10 @@ function handleMessage(data, socket) {
         handleError(socket, '5102')
         throw new SyntaxError('(handleMessage) Arguments Error: missing second param')
       }
-      handleAccept.bind(this, socket)(message)
+      handleAccept.bind(this, socket)(message, success)
       break
     case eventType.hangup:
       handleHangup.bind(this, socket)(message)
-      break
-    case eventType.leave:
-      handleLeave.bind(this, socket)(message)
       break
     default:
       handleError(socket, 5103)
@@ -105,11 +103,11 @@ function handleCall(socket, uuid) {
     if (customUser.calling) {
       throw new Error(5004)
     }
+    if (!connectedUser) {
+      throw new Error(5003)
+    }
     if (connectedUser.calling) {
       throw new Error(5001)
-    }
-    if (!connectedUser.socket) {
-      throw new Error(5003)
     }
     customUser.calling = true // 更改状态
     customUser.setConnectedUser(connectedUser)
@@ -121,7 +119,7 @@ function handleCall(socket, uuid) {
   }
 }
 
-function handleAccept(socket, uuid) {
+function handleAccept(socket, uuid, success) {
   try {
     const customUser = queueInstance.findBySocket(socket)
     const connectedUser = queueInstance.findByUuid(uuid)
@@ -135,22 +133,23 @@ function handleAccept(socket, uuid) {
     if (!connectedUser.calling) {
       throw new Error(5002)
     }
-    customUser.calling = true // 更改状态
-    connectedUser.socket.send({ event: 'accept', message: customUser._uuid })
-    console.log(`User "${customUser.username}" had accepted...`)
-    // 在各自信息中缓存对方的信息
-    customUser.setConnectedUser(connectedUser)
+    if (success) {
+      customUser.calling = true // 更改状态
+      connectedUser.socket.send({ event: 'accept', message: customUser._uuid, success: true })
+      console.log(`User "${customUser.username}" had accepted...`)
+      // 在各自信息中缓存对方的信息
+      customUser.setConnectedUser(connectedUser)
+    } else {
+      connectedUser.socket.send({ event: 'reject', message: 5006 })
+      console.log(`User "${customUser.username}" had turned down ${connectedUser.username}'s call...`)
+    }
   } catch (err) {
     const message = +err.message
     socket.send({ event: 'reject', message })
   }
 }
 
-function handleHangup() {
-
-}
-
-function handleLeave(socket) {
+function handleHangup(socket) {
   const customUser = queueInstance.findBySocket(socket)
   try {
     if (!customUser.calling) {
@@ -164,7 +163,7 @@ function handleLeave(socket) {
         // 双方通话中
         connectedUser.calling = false
         connectedUser.setConnectedUser(null)
-        connectedUser.socket.send({ event: 'leave' })
+        connectedUser.socket.send({ event: 'hangup' })
         console.log(`User ${connectedUser.username} had stopped Chatting together`)
       }
     }
@@ -231,14 +230,14 @@ module.exports = function(server) {
             const del = queueInstance.deleteBySocket(socket)
 
             if (del.calling) {
-              const { connectedUser } = del
+              const connectedUser = del.getConnectedUser()
               // 判断是否有对方信息
               if (connectedUser) {
                 // 如果正在通话中，告诉对方已经离开
                 const otherSocket = connectedUser.socket
                 connectedUser.calling = false
                 connectedUser.setConnectedUser(null)
-                otherSocket.send({ event: 'leave', message: reason })
+                otherSocket.send({ event: 'hangup', message: reason })
               }
             }
 
